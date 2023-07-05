5ee21ec318c228e8277973539e15fefe48fb155c
Martin Ashworth <martin.ashworth@icloud.com>
2023-07-05 23:14:26 +0100

    refectored play.py

--- play.py
+++ play.py
@@ -10,6 +10,7 @@ SUN_API_BASE_URL = 'https://api.sunrise-sunset.org/json'
 TIMEZONE = 'Europe/London'
 TIDE_URL = 'https://www.tidetime.org/europe/united-kingdom/portobello.htm'
 
+
 def get_request(url):
     """
     Perform a GET request to a given URL.
@@ -23,6 +24,7 @@ def get_request(url):
         print(f"Request Error: {e}")
         exit(1)
 
+
 def fetch_tide_data(tide_url):
     """
     Fetch the tide data from the given URL.
@@ -31,9 +33,10 @@ def fetch_tide_data(tide_url):
     tide_response = get_request(tide_url)
     soup = BeautifulSoup(tide_response.text, 'html.parser')
     tide_table = soup.find('table', {'id': 'tideTable'})
-    tr_results = tide_table.find_all('tr')
+    tr_results = tide_table.find_all('tr')   # type: ignore
     columns = tr_results[1].find_all('td')
     return columns
+
 
 def fetch_sun_data(cdate, lat, long):
     """
@@ -43,21 +46,24 @@ def fetch_sun_data(cdate, lat, long):
     response = get_request(url)
     return response.json()
 
+
 def format_sun_time(sun_time):
     """
-    Convert a time string from the sunrise-sunset API into a formatted string in local time.
+    Convert time string from sunrise-sunset API to string in local time.
     """
     dt = datetime.fromisoformat(sun_time).astimezone(pytz.timezone(TIMEZONE))
     return dt.strftime('%H:%M')
 
+
 def get_sun_times(cdate):
     """
-    Fetch the sunrise and sunset times for a given date, and return them as formatted strings.
+    Fetch sunrise and sunset times for a given date, return as strings.
     """
     sun_data = fetch_sun_data(cdate, LATITUDE, LONGITUDE)
     sunrise = format_sun_time(sun_data['results']['sunrise'])
     sunset = format_sun_time(sun_data['results']['sunset'])
     return sunrise, sunset
+
 
 # Fetch tide data
 tide_data = fetch_tide_data(TIDE_URL)
@@ -77,5 +83,5 @@ for each_day in tide_data:
         print(f"{tide_time} - {tide_type}")
 
     # Advance to the next day
-    cdate = cdate + timedelta(days = 1)
+    cdate = cdate + timedelta(days=1)
     print()
