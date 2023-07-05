33c01cb92fc5f04855fefb240476e5d8bcc58826
Martin Ashworth <martin.ashworth@icloud.com>
2023-07-06 00:18:13 +0100

    weather introduction

--- play.py
+++ play.py
@@ -65,23 +65,77 @@ def get_sun_times(cdate):
     return sunrise, sunset
 
 
-# Fetch tide data
-tide_data = fetch_tide_data(TIDE_URL)
-cdate = date.today()
+###############################################################################
+# Weather - Make an API call to fetch the hourly temperature data
+###############################################################################
 
-# Loop over each day's tide data
-for each_day in tide_data:
-    # Fetch and print sunrise and sunset times
-    sunrise, sunset = get_sun_times(cdate)
-    print(f"{cdate.strftime('%A')} - Sunrise: {sunrise} - Sunset: {sunset}")
+url = f'https://api.open-meteo.com/v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&hourly=temperature_2m,weathercode&timezone=GMT'
+response = requests.get(url)
+weather = response.json()   # supplied as a dictionary with hourly data
+hourly_data = weather['hourly']
+# print(hourly_data.keys())
+temperature_2m = hourly_data['temperature_2m']
+weathercode = hourly_data['weathercode']
+timestamps = hourly_data['time']
 
-    # Find and print each tide time and type
-    tides = each_day.find_all('li', {'class': ['highTide', 'lowTide']})
-    for tide in tides:
-        tide_time = tide.find('strong').text.split(' ')[1]
-        tide_type = tide.find('span', {'class': 'tidal-state'}).text
-        print(f"{tide_time} - {tide_type}")
+# print(timestamps)
+# print(len(timestamps))
 
-    # Advance to the next day
-    cdate = cdate + timedelta(days=1)
-    print()
+daily_temperatures = {}
+
+for i, temp in enumerate(temperature_2m):
+    dt = datetime.fromisoformat(timestamps[i])
+    date_str = dt.strftime('%Y-%m-%d')
+    hour = dt.hour
+    # print(i, temp, hour)
+
+# ################################################################################
+# # Consider daytime hours between 6 AM and 6 PM
+# ################################################################################
+
+    if 8 <= hour < 20:
+        if date_str not in daily_temperatures:
+            daily_temperatures[date_str] = {'total_temp': 0, 'count': 0, 'weathercode': weathercode[i]}
+            print(daily_temperatures)
+        daily_temperatures[date_str]['total_temp'] += temp
+        daily_temperatures[date_str]['count'] += 1
+
+print(daily_temperatures)
+
+# ################################################################################
+# # Calculate the average daytime temperature
+# ################################################################################
+
+#     for date_str in daily_temperatures:
+#         total_temp = daily_temperatures[date_str]['total_temp']
+#         count = daily_temperatures[date_str]['count']
+#         average_temp = total_temp / count
+#         daily_weathercode = daily_temperatures[date_str]['weathercode']
+        
+################################################################################
+# print(f"{date_str}: {average_temp:.1f}°C, weathercode: {daily_weathercode}")
+################################################################################
+###############################################################################
+# the following works fine
+###############################################################################
+# # Fetch tide data
+# tide_data = fetch_tide_data(TIDE_URL)
+# cdate = date.today()
+
+# # Loop over each day's tide data
+# for each_day in tide_data:
+#     # Fetch and print sunrise and sunset times
+#     sunrise, sunset = get_sun_times(cdate)
+#     print(f"{cdate.strftime('%A')} - Sunrise: {sunrise} - Sunset: {sunset}")
+
+#     # Find and print each tide time and type
+#     tides = each_day.find_all('li', {'class': ['highTide', 'lowTide']})
+#     for tide in tides:
+#         tide_time = tide.find('strong').text.split(' ')[1]
+#         tide_type = tide.find('span', {'class': 'tidal-state'}).text
+#         print(f"{tide_time} - {tide_type}")
+
+#     # Advance to the next day
+#     cdate = cdate + timedelta(days=1)
+#     print()
+
